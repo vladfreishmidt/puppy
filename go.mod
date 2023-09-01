892ceb0c81c9e17d81d7f19de4b39ba4d25490ed
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-09-01 15:59:38 +0300

    Add BigBark func

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vladfreishmidt/puppy
 
 go 1.21.0
+
+require github.com/vladfreishmidt/dog v0.0.0-20230901113912-7cb42dceaf10 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/vladfreishmidt/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
